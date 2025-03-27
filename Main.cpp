0bf82a199a222bd25ca7ff8f6cc469327016af7a
Roman <romannsergeev@gmail.com>
2025-03-27 05:43:00 +0500

    Forgot to stage files in VS Code

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,34 @@
-#include <stdio.h>
+#include "include/glew/glew.h"
+#include "include/glfw/glfw3.h"
 #include <iostream>
 
 int main() {
-    printf("Hello world\n");
-    int a;
-    scanf("%d", &a);
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
