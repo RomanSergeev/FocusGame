12e6740137164ad54ece01e94275e87e6624f2a6
Roman <romannsergeev@gmail.com>
2025-10-23 04:56:31 +0500

    cmake update (4)
    Polishing - grouped CMakeLists commands, removed MinGW includes + absolute paths (already in the toolchain), removed linker hardcoded path (now rests in clang++.cfg).
    Removed all MinGW local paths - now also in clang++.cfg.

--- toolchain-mingw.cmake
+++ toolchain-mingw.cmake
@@ -1,35 +1,15 @@
-# Compiler locations
-set(CLANG_ROOT "E:/Programs/CLang/bin")
-set(MINGW_ROOT "E:/Programs/CMinGW")
-set(MINGW_LIBS "${MINGW_ROOT}/lib/gcc/x86_64-w64-mingw32/14.2.0")
-set(MINGW_CPP  "${MINGW_ROOT}/include/c++/14.2.0")
+# Removed CMAKE_EXE_LINKER_FLAGS_INIT, CMAKE_<CONFIG>_LINKER_FLAGS_INIT "-fuse-ld=fullpath/ld.lld.exe"
+# clang++.cfg keeps path to the correct linker (I had to force use of ld.lld.exe)
+# wrong target compiler mode treats linker flags differently
+# Removed filesystem-specific MinGW standard library paths - now in clang++.cfg
 
-# Force Clang to use lld.exe instead of lld-link.exe
-set(CMAKE_LINKER "${CLANG_ROOT}/ld.lld.exe" CACHE STRING "Force use of ld.lld.exe" FORCE)
+# Make sure CLang/bin is in your PATH
+set(CMAKE_C_COMPILER "clang.exe" CACHE FILEPATH "C compiler path" FORCE)
+set(CMAKE_CXX_COMPILER "clang++.exe" CACHE FILEPATH "C++ compiler path" FORCE)
 
-# Set path to the compiler
-set(CMAKE_C_COMPILER "${CLANG_ROOT}/clang.exe" CACHE STRING "C compiler path" FORCE)
-set(CMAKE_CXX_COMPILER "${CLANG_ROOT}/clang++.exe" CACHE STRING "C++ compiler path" FORCE)
-
-# NEW iostream issue: flags
-# this setup is extremely tight on all the library paths. Might move all the settings to clang++.cfg, but keep it here for one commit for reference.
-set(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS} -nostdinc \
--isystem${MINGW_ROOT}/include \
--isystem${MINGW_ROOT}/x86_64-w64-mingw32/include")
-
-set(CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS} -nostdinc++ \
--isystem${MINGW_CPP} \
--isystem${MINGW_CPP}/x86_64-w64-mingw32 \
--isystem${MINGW_ROOT}/x86_64-w64-mingw32/include")
-
-# Target triple (force MinGW GNU ABI)
+# Target triple (force MinGW GNU ABI (Application Binary Interface))
 set(CMAKE_C_COMPILER_TARGET "x86_64-w64-mingw32")
 set(CMAKE_CXX_COMPILER_TARGET "x86_64-w64-mingw32")
-
-# Set proper linker flags - won't work (will be overridden) without specifying the target compiler mode first
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=${CMAKE_LINKER}" CACHE STRING "" FORCE)
-#set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=${CMAKE_LINKER}" CACHE STRING "" FORCE)
-#set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=${CMAKE_LINKER}" CACHE STRING "" FORCE)
 
 # Override CMake's default linking behavior
 set(CMAKE_C_LINK_EXECUTABLE "<CMAKE_C_COMPILER> -o <TARGET> <OBJECTS> ${CMAKE_EXE_LINKER_FLAGS} <LINK_LIBRARIES>")
