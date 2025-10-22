931dc01baafd229836c3eb862a2428e37141cfcc
Roman <romannsergeev@gmail.com>
2025-10-22 19:56:14 +0500

    cmake update (2)
    Moved toolchain-related flags on top, followed the order of compiler->its flags->linker flags->executable flags.

--- toolchain-mingw.cmake
+++ toolchain-mingw.cmake
@@ -3,3 +3,6 @@ set(CLANG_ROOT "E:/Programs/CLang/bin")
 set(MINGW_ROOT "E:/Programs/CMinGW")
 set(MINGW_LIBS "${MINGW_ROOT}/lib/gcc/x86_64-w64-mingw32/14.2.0")
 set(MINGW_CPP  "${MINGW_ROOT}/include/c++/14.2.0")
+
+# Force Clang to use lld.exe instead of lld-link.exe
+set(CMAKE_LINKER "${CLANG_ROOT}/ld.lld.exe" CACHE STRING "Force use of ld.lld.exe" FORCE)
